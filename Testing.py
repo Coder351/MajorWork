8f70220ea15127a5e21fc77cc80be3ebce2e51db
Mrinaank SInha <car.knives@gmail.com>
2018-04-26 04:35:27 +1000

    Created TODO items and tested with different cases

--- Testing.py
+++ Testing.py
@@ -7,13 +7,13 @@ import os
 import numpy as np
 from pprint import pprint
 
+#TODO:create own .trainneddata that can be used with below preprocesing for all types
 def preprocess(img):
     img = cv2.GaussianBlur(img,(3,3),0)
-    img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV,75,20)
+    img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV,55,10)
 
-    # crop to smallest portion
-    _,contours,hierachy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_TC89_KCOS)
-
+    #getting cropped image
+    _, contours, hierachy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_KCOS)
 
     points = np.concatenate(contours)
 
@@ -23,26 +23,16 @@ def preprocess(img):
     if angle < -45.0:
         angle += 90.0
 
-    rotMatrix = cv2.getRotationMatrix2D(rotatedRect[0],angle,1)
-    rotated = cv2.warpAffine(img,rotMatrix,img.shape[0:2][::-1],flags=cv2.INTER_CUBIC)
+    rotMatrix = cv2.getRotationMatrix2D(rotatedRect[0], angle, 1)
+    img = cv2.warpAffine(img, rotMatrix, img.shape[0:2][::-1], flags=cv2.INTER_CUBIC)
 
     if rotatedRect[2] < -45.0:
-        w,h = rotatedRect[1][::-1]
+        w, h = rotatedRect[1][::-1]
     else:
-        w,h = rotatedRect[1]
+        w, h = rotatedRect[1]
 
-
-    cropped = cv2.getRectSubPix(rotated,(int(w),int(h)),rotatedRect[0])
-
-
-    return cropped
-
-
-
-
-
-
-
+    img = cv2.getRectSubPix(img, (int(w), int(h)), rotatedRect[0])
+    return img
 
 # construct the argument parse and parse the arguments
 # ap = argparse.ArgumentParser()
@@ -56,21 +46,22 @@ def preprocess(img):
 filepath = input("Enter filepath: ") #example1.jpg
 image = cv2.imread(filepath)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# gray = cv2.bitwise_not(gray) TODO: get selective cases for when to apply and not apply this
 
-preprocess(gray)
+preprocessedImg = preprocess(gray)
 
 # write the grayscale image to disk as a temporary file so we can
 # apply OCR to it
 filename = "{}.png".format(os.getpid())
-cv2.imwrite(filename, gray)
+cv2.imwrite(filename, preprocessedImg)
 
 # load the image as a PIL/Pillow image, apply OCR, and then delete
 # the temporary file
 text = pytesseract.image_to_string(Image.open(filename),lang="eng")
-os.remove(filename)
+os.remove(filename) #TODO: check if this works
 print(text)
 
 # show the output images
-cv2.imshow("Image", image)
-cv2.imshow("Output", gray)
+cv2.imshow("Image", gray)
+cv2.imshow("Output", preprocessedImg)
 cv2.waitKey(0)
